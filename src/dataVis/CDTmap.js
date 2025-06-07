c7ea5f5d8396cb4bbc14966f1338b2a40ba4cda0
Katherine Wyman <katy6514@gmail.com>
2025-06-07 11:44:19 -0400

    fixed so zoom works on correct state

--- src/dataVis/CDTmap.js
+++ src/dataVis/CDTmap.js
@@ -1,7 +1,7 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState, useMemo } from "react";
 import * as d3 from "d3";
 
-import { width, height, path, projection, cities } from "./constants.js";
+import { width, height, cities } from "./constants.js";
 
 import {
   getAlternatingColor,
@@ -14,10 +14,17 @@ import {
 const CDTmap = () => {
   const ref = useRef();
 
-  useEffect(() => {
-    // Clear previous chart if any
-    d3.select(ref.current).selectAll("*").remove();
+  // ✅ Define projection + path WITHIN component and memoize
+  const projection = useMemo(() => {
+    return d3
+      .geoAlbersUsa()
+      .scale(2000)
+      .translate([width * 0.9, height * 0.625]);
+  }, []);
 
+  const path = useMemo(() => d3.geoPath().projection(projection), [projection]);
+
+  useEffect(() => {
     const svg = d3
       .select(ref.current)
       .attr("id", "CDTmap")
@@ -26,14 +33,109 @@ const CDTmap = () => {
       .attr("stroke", "rgb(127, 127, 127)")
       .attr("stroke-width", "1px");
 
+    svg.selectAll("*").remove();
+
+    const g = svg.append("g").attr("class", "mapLayer");
+
+    // Add zoom behavior
+    const zoom = d3
+      .zoom()
+      .scaleExtent([1, 500])
+      .on("zoom", (event) => {
+        g.attr("transform", event.transform);
+        g.selectAll("circle").attr("r", 4 / event.transform.k);
+        g.selectAll("text").attr("font-size", 12 / event.transform.k);
+        g.selectAll("line").attr("stroke-width", 1 / event.transform.k);
+        g.selectAll(".trail").attr("stroke-width", 2 / event.transform.k);
+      });
+
+    svg.call(zoom);
+
+    svg.on("click", (event) => {
+      if (!event.target.classList.contains("state-clickable")) {
+        svg.transition().duration(750).call(zoom.transform, d3.zoomIdentity);
+      }
+    });
+
+    // // Set up the zoom behavior
+    // const zoom = d3
+    //   .zoom()
+    //   .scaleExtent([1, 500]) // Limits of the zoom scale
+    //   .on("zoom", (event) => {
+    //     const { transform } = event;
+    //     const scale = transform.k;
+
+    //     // d3.select("#CDTmap").attr("transform", transform);
+    //     svg.selectAll("circle").attr("transform", transform); // Apply transform on zoom
+    //     svg.selectAll("path").attr("transform", transform); // Apply transform on zoom
+    //     svg.selectAll("text").attr("transform", transform); // Apply transform on zoom
+    //     svg.selectAll("image").attr("transform", transform); // Apply transform on zoom
+    //     svg.selectAll("line").attr("transform", transform); // Apply transform on zoom
+
+    //     // Adjust point sizes inversely to zoom
+    //     d3.selectAll("circle").attr("r", 4 / scale);
+    //     labels.attr("font-size", 12 / scale);
+    //     d3.selectAll("line").attr("stroke-width", 1 / scale);
+    //     d3.selectAll(".trail").attr("stroke-width", 2 / scale);
+    //   });
+
+    // svg.call(zoom);
+
+    // // Add background click to reset zoom
+    // svg.on("click", (event) => {
+    //   if (!event.target.classList.contains("state-clickable")) {
+    //     // Check if clicked outside a state
+    //     svg.transition().duration(750).call(zoom.transform, d3.zoomIdentity); // Reset zoom to initial state
+    //   }
+    // });
+
+    /* -----------------------------------------------------
+    *  State outline mapping functionality
+    ----------------------------------------------------- */
+    // geojson data from: https://github.com/johan/world.geo.json/tree/master
+    d3.json("CDTstates.json").then((stateData) => {
+      g.selectAll(".state")
+        .data(stateData.features)
+        .enter()
+        .append("path")
+        .attr("class", "state state-clickable")
+        .attr("fill", "transparent")
+        .attr("stroke", "gray")
+        .attr("stroke-width", "1px")
+        .attr("d", path)
+        // .lower()
+        .on("click", function (event, d) {
+          event.stopPropagation();
+
+          // Check if we’re already zoomed in on this state
+          const [[x0, y0], [x1, y1]] = path.bounds(d); // Get bounding box of the selected state
+          const dx = x1 - x0;
+          const dy = y1 - y0;
+          const x = (x0 + x1) / 2;
+          const y = (y0 + y1) / 2;
+          const scale = Math.max(
+            1,
+            Math.min(8, 0.9 / Math.max(dx / width, dy / height))
+          );
+          const translate = [width / 2 - scale * x, height / 2 - scale * y];
+
+          svg
+            .transition()
+            .duration(750)
+            .call(
+              zoom.transform,
+              d3.zoomIdentity.translate(translate[0], translate[1]).scale(scale)
+            );
+        });
+    });
+
     /* -----------------------------------------------------
     *  Plotting route Data
     ----------------------------------------------------- */
-    d3.json("/CDT_border_to_lincoln.json").then((data) => {
+    d3.json("/CDT_border_to_lincoln.json").then((trailData) => {
       // Plot the points
-      svg
-        .selectAll(".trail")
-        .data(data.features)
+      g.selectAll(".trail")
+        .data(trailData.features)
         .enter()
         .append("path")
         .attr("class", "trail")
@@ -49,21 +151,19 @@ const CDTmap = () => {
     /* -----------------------------------------------------
     *  Plotting Garmin Data
     ----------------------------------------------------- */
-    d3.json("cdtInreachData_withCoords.geojson").then((data) => {
-      const points = data.features.filter(
+    d3.json("cdtInreachData_withCoords.geojson").then((inReachdata) => {
+      const points = inReachdata.features.filter(
         (d) =>
           d.geometry?.type === "Point" &&
           Array.isArray(d.geometry.coordinates) &&
           d.geometry.coordinates.length === 2
       );
-      const validPoints = points.filter((d) => {
-        const projected = projection(d.geometry.coordinates);
-        return projected != null;
-      });
+      const validPoints = points.filter((d) =>
+        projection(d.geometry.coordinates)
+      );
 
       // Plot the points
-      svg
-        .selectAll(".points")
+      g.selectAll(".points")
         .data(validPoints)
         .enter()
         .append("circle")
@@ -79,34 +179,13 @@ const CDTmap = () => {
     });
 
     /* -----------------------------------------------------
-    *  State outline mapping functionality
-    ----------------------------------------------------- */
-    // geojson data from: https://github.com/johan/world.geo.json/tree/master
-    d3.json("CDTstates.json").then((data) => {
-      svg
-        .selectAll(".state")
-        .data(data.features)
-        .enter()
-        .append("path")
-        .attr("class", "state")
-        .attr("fill", "rgba(255, 255, 255, 0)")
-        .attr("stroke", "rgb(127, 127, 127)")
-        .attr("stroke-width", "1px")
-        .attr("d", path)
-        .lower()
-        .on("click", clicked);
-    });
-
-    /* -----------------------------------------------------
     *  Take the cleaned photo geojson data and plot it
     ----------------------------------------------------- */
     d3.json("photoData.json").then((photoData) => {
-      const validPoints = photoData.filter((d) => {
-        const projected = projection([d.longitude, d.latitude]);
-        return projected != null;
-      });
-      svg
-        .selectAll(".photoPoints")
+      const validPoints = photoData.filter((d) =>
+        projection([d.longitude, d.latitude])
+      );
+      g.selectAll(".photoPoints")
         .data(validPoints)
         .enter()
         .append("circle")
@@ -121,7 +200,11 @@ const CDTmap = () => {
         .on("mouseout", handleMouseOut);
     });
 
-    const cityGroup = svg.append("g").attr("class", "cities");
+    /* -----------------------------------------------------
+    *  City data
+    ----------------------------------------------------- */
+
+    const cityGroup = g.append("g").attr("class", "cities");
 
     cityGroup
       .selectAll("circle")
@@ -146,7 +229,7 @@ const CDTmap = () => {
       .attr("stroke", "black")
       .attr("stroke-width", 1);
 
-    const labels = cityGroup
+    cityGroup
       .selectAll("text")
       .data(cities)
       .enter()
@@ -159,63 +242,7 @@ const CDTmap = () => {
       .attr("text-anchor", (d) => (d.dx <= 0 ? "end" : "start"))
       .attr("fill", "black")
       .attr("stroke", "none");
-
-    function clicked(event, d) {
-      console.log({ d });
-      // Check if we’re already zoomed in on this state
-      const [[x0, y0], [x1, y1]] = path.bounds(d); // Get bounding box of the selected state
-      const dx = x1 - x0;
-      const dy = y1 - y0;
-      const x = (x0 + x1) / 2;
-      const y = (y0 + y1) / 2;
-      const scale = Math.max(
-        1,
-        Math.min(8, 0.9 / Math.max(dx / width, dy / height))
-      );
-      const translate = [width / 2 - scale * x, height / 2 - scale * y];
-
-      svg
-        .transition()
-        .duration(750)
-        .call(
-          zoom.transform,
-          d3.zoomIdentity.translate(translate[0], translate[1]).scale(scale)
-        );
-    }
-
-    // Set up the zoom behavior
-    const zoom = d3
-      .zoom()
-      .scaleExtent([1, 500]) // Limits of the zoom scale
-      // .on("zoom", handleZoom);
-      .on("zoom", (event) => {
-        const { transform } = event;
-        const scale = transform.k;
-
-        // d3.select("#CDTmap").attr("transform", transform);
-        svg.selectAll("circle").attr("transform", transform); // Apply transform on zoom
-        svg.selectAll("path").attr("transform", transform); // Apply transform on zoom
-        svg.selectAll("text").attr("transform", transform); // Apply transform on zoom
-        svg.selectAll("image").attr("transform", transform); // Apply transform on zoom
-        svg.selectAll("line").attr("transform", transform); // Apply transform on zoom
-
-        // Adjust point sizes inversely to zoom
-        d3.selectAll("circle").attr("r", 4 / scale);
-        labels.attr("font-size", 12 / scale);
-        d3.selectAll("line").attr("stroke-width", 1 / scale);
-        d3.selectAll(".trail").attr("stroke-width", 2 / scale);
-      });
-
-    svg.call(zoom);
-
-    // Add background click to reset zoom
-    svg.on("click", (event) => {
-      if (event.target.tagName !== "path") {
-        // Check if clicked outside a state
-        svg.transition().duration(750).call(zoom.transform, d3.zoomIdentity); // Reset zoom to initial state
-      }
-    });
-  }, []); // run once on mount
+  }, [path, projection]); // run once on mount
 
   return <svg ref={ref}></svg>;
 };
