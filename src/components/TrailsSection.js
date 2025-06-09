72119faab6a065358cdc5daff8725fd82d2de7cc
Katherine Wyman <katy6514@gmail.com>
2025-06-09 14:47:08 -0400

    html and css flesh out

--- src/components/TrailsSection.js
+++ src/components/TrailsSection.js
@@ -6,7 +6,36 @@ import TrailsTable from "./TrailsTable.js";
 export default function TrailsSection({ user }) {
   return (
     <>
-      <section class="wrapper style1 align-center">
+      <section
+        class="spotlight addBoxShadow style1 orient-right content-align-left image-position-center onscroll-image-fade-in"
+        id="first"
+        // style={{ boxShadow: "inset 0 1px 0 0 rgba(0, 0, 0, 0.075)" }}
+      >
+        <div class="content">
+          <h2>Legs</h2>
+          <p>
+            I saved the location of my nightly campsites with my garmin. I did
+            not record my walks each day with the garmin becuase I didn't want
+            to drain the battery. Instead I used the saved locations of my
+            campsites and recreated the route I took each day with Caltopo. I
+            then exported those tracks as GeoJSON and uploaded them here.
+          </p>
+          <ul class="actions stacked">
+            <li>
+              <a href="#" class="button">
+                Caltopo link?
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="image">
+          <img src="images/spotlight01.jpg" alt="" />
+        </div>
+      </section>
+      <section
+        class="wrapper style1 align-center"
+        // style={{ boxShadow: "inset 0 1px 0 0 rgba(0, 0, 0, 0.075)" }}
+      >
         <div class="inner">
           <UploadDialog uploadType={dataTypes.trail} user={user} />
           <TrailsTable />
